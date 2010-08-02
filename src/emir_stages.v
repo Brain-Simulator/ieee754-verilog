03a8f4f3b98fad9e7a1ed51b30d944445cf0910b
Emir Habul <emiraga@gmail.com>
2010-08-02 13:46:23 +0100

    Started generator

---
+++ src/emir_stages.v
@@ -0,0 +1,21 @@
+
+module emir_module3(C,EA,EB,Z);
+	input C;
+	input EA;
+	input EB;
+	output Z;
+endmodule
+
+module emir_module2(input [0:2] D, output E);
+
+endmodule
+
+module emir_module1(A,B,C,D2,D1,Y);
+	input A;
+	input B;
+	output C;
+	output [0:2] D1;
+	output [0:2] D2;
+	output Y;
+endmodule
+
