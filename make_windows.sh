5a92b1d107b3b40889062997d4fdbeda31c55bf1
Emir Habul <emiraga@gmail.com>
2010-08-05 18:51:58 +0100

    make script

--- make_windows.sh
+++ make_windows.sh
@@ -1,24 +1,26 @@
 #/bin/sh
 #
-# Used on windows together with:
-# * msysgit  - http://code.google.com/p/msysgit/
-# * iverilog - http://bleyer.org/icarus/
-# * python   - http://www.python.org/
+# Used on MS Windows together with:
 #
-
+# * msysgit       - http://code.google.com/p/msysgit/
+# * iverilog      - http://bleyer.org/icarus/
+# * python        - http://www.python.org/
+# * verilogscript - http://github.com/emiraga/verilogscript
+#
+echo 'Configuring environment'
 export PATH=$PATH:/x/verilog/iverilog/bin:/c/python27/
+export VSCRIPT=/x/VerilogScript/VerilogScript.py
 
-python /x/VerilogScript/VerilogScript.py src/ieee.vs || exit
-
+echo 'generating pipeline'
+python $VSCRIPT src/ieee.vs || exit
 cd src
 python generate.py || exit
 cd ..
-
 rm src/ieee.v
 
 echo 'Compiling verilog'
 rm -f bin/single.out
-python /x/VerilogScript/VerilogScript.py src/ieee.vs src/ieee_adder.v test/testbench.v -e "iverilog -o bin/single.out" || exit
+python $VSCRIPT src/ieee.vs src/ieee_adder.v test/testbench.v -e "iverilog -o bin/single.out" || exit
 
 echo 'Running simulation'
 rm -f bin/simulation.out
@@ -30,4 +32,3 @@ gcc test/test_simulation.c -o bin/test.exe || exit
 
 echo 'Running test'
 bin/test.exe < bin/simulation.out
-
